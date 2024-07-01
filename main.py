10f22f162e1e10b0a47070c017c5472a2822a4a2
FireLynx <mikaelbaath@gmail.com>
2024-07-01 11:45:55 +0200

    refactor template into jinja2

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 
 from models import Character, ActivityEnum, HomeEnum, MoralDilemmaEnum, AdventureEnum, DemeanorEnum
 from services.backstory.service import BackstoryService
@@ -17,51 +19,20 @@ class CharacterAnswers(BaseModel):
     demeanor: str
 
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/", response_class=HTMLResponse)
-async def get_form():
-    activity_options = "</option><option value=\"".join([f"{activity.name}\">{activity.value}" for activity in ActivityEnum])
-    home_options = "</option><option value=\"".join([f"{home.name}\">{home.value}" for home in HomeEnum])
-    moral_dilemma_options = "</option><option value=\"".join([f"{moral_dilemma.name}\">{moral_dilemma.value}" for moral_dilemma in MoralDilemmaEnum])
-    adventure_options = "</option><option value=\"".join([f"{adventure.name}\">{adventure.value}" for adventure in AdventureEnum])
-    demeanor_options = "</option><option value=\"".join([f"{demeanor.name}\">{demeanor.value}" for demeanor in DemeanorEnum])
+async def get_form(request: Request):
+    context = {
+        "request": request,
+        "activities": ActivityEnum,
+        "homes": HomeEnum,
+        "dilemmas": MoralDilemmaEnum,
+        "adventures": AdventureEnum,
+        "demeanors": DemeanorEnum
+    }
+    return templates.TemplateResponse("form.html", context=context, media_type="text/html")
 
-    html_content = f"""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Create a DnD Character</title>
-        <script src="static/formHandler.js"></script>
-    </head>
-    <body>
-        <h1>Create Your DnD Character</h1>
-        <form action="/create-character/" method="post" id="characterForm">
-            <label for="activity">Which of these activities would your character excel at during their free time?</label><br>
-            <select id="activity" name="activity">
-                {activity_options}
-
-            </select><br><br>
-            <label for="home">Where did your character grow up?</label><br>
-            <select id="home" name="home">
-                {home_options}
-            </select><br><br>
-            <label for="moral_dilemma">How does your character approach a moral dilemma?</label><br>
-            <select id="moral_dilemma" name="moral_dilemma">
-                {moral_dilemma_options}
-            </select><br><br>
-            <label for="adventure">What motivates your character to embark on an adventure?</label><br>
-            <select id="adventure" name="adventure">
-                {adventure_options}
-            </select><br><br>
-            <label for="demeanor">Which of these traits best describes your character's demeanor?</label><br>
-            <select id="demeanor" name="demeanor">
-                {demeanor_options}
-            </select><br><br>
-            <input type="submit" value="Create Character">
-        </form>
-    </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
 
 
 @app.get("/static/formHandler.js")
